0879c1d6719387b93b9f3db1d4f5f0e3af300d80
Patrick Smith <patrick@burntcaramel.com>
2017-11-08 15:38:58 +1100

    Moar comments™

--- server.js
+++ server.js
@@ -19,21 +19,22 @@ const server = HTTP.createServer((request, response) => {
     response.end('THIS IS THE ROOT')
   }
   else if (path === '/random') {
+    // Generate a random number
     response.end(`${Math.random()}`)
   }
   else if (path === '/movies') {
     if (request.method === 'POST') {
       // Create
       readBodyAsJSON(request, (error, newMovie) => {
-        if (error) {
-          // Error parsing JSON
+        if (error) { // Error parsing JSON
           sendJSON(response, {
             "error": error.message
           }, 400)
         }
-        else {
-          // Successfully got JSON
+        else { // Successfully got JSON
+          // Add the new movie to our array
           movies.push(newMovie)
+          // Send back the new movie with 201 Created status
           sendJSON(response, newMovie, 201)
         }
       })
@@ -48,6 +49,8 @@ const server = HTTP.createServer((request, response) => {
     }
   }
   else {
+    // Unknown path
+    // 404 Not Found
     response.writeHead(404)
     response.end(`The route '${path}' does not exist`)
   }
